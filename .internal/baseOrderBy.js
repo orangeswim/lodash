bd9dcf42271786284d3998c511093b57a399a683
Cyprian Bergonia <orangeswimstudios@gmail.com>
2022-07-03 14:41:17 -0500

    base

--- .internal/baseOrderBy.js
+++ .internal/baseOrderBy.js
@@ -1,10 +1,11 @@
-import baseEach from './baseEach.js'
-import baseSortBy from './baseSortBy.js'
-import baseGet from './baseGet.js'
-import compareMultiple from './compareMultiple.js'
-import isArrayLike from '../isArrayLike.js'
+import baseEach from "./baseEach.js";
+import baseSortBy from "./baseSortBy.js";
+import baseGet from "./baseGet.js";
+import compareMultiple from "./compareMultiple.js";
+import isArrayLike from "../isArrayLike.js";
+import baseUnary from "../modularize/_baseUnary.js";
 
-const identity = (value) => value
+const identity = (value) => value;
 
 /**
  * The base implementation of `orderBy` without param guards.
@@ -12,38 +13,44 @@ const identity = (value) => value
  * @private
  * @param {Array|Object} collection The collection to iterate over.
  * @param {Function[]|Object[]|string[]} iteratees The iteratees to sort by.
- * @param {string[]} orders The sort orders of `iteratees`.
+ * @param {string[]|Function[]} orders The sort orders of `iteratees`.
  * @returns {Array} Returns the new sorted array.
  */
 function baseOrderBy(collection, iteratees, orders) {
   if (iteratees.length) {
     iteratees = iteratees.map((iteratee) => {
       if (Array.isArray(iteratee)) {
-        return (value) => baseGet(value, iteratee.length === 1 ? iteratee[0] : iteratee)
+        return (value) =>
+          baseGet(
+            value,
+            iteratee.length === 1 ? iteratee[0] : baseUnary(iteratee)
+          );
       }
 
-      return iteratee
-    })
+      return iteratee;
+    });
   } else {
-    iteratees = [identity]
+    iteratees = [identity];
   }
 
-  let criteriaIndex = -1
-  let eachIndex = -1
+  let criteriaIndex = -1;
+  let eachIndex = -1;
 
-  const result = isArrayLike(collection) ? new Array(collection.length) : []
+  const result = isArrayLike(collection) ? new Array(collection.length) : [];
 
   baseEach(collection, (value) => {
-    const criteria = iteratees.map((iteratee) => iteratee(value))
+    const criteria = iteratees.map((iteratee) => iteratee(value));
 
     result[++eachIndex] = {
       criteria,
       index: ++criteriaIndex,
-      value
-    }
-  })
+      value,
+    };
+  });
 
-  return baseSortBy(result, (object, other) => compareMultiple(object, other, orders))
+  return baseSortBy(result, (object, other) =>
+    compareMultiple(object, other, orders)
+  );
 }
 
-export default baseOrderBy
+export default baseOrderBy;
